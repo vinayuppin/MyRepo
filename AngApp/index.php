c017b0fe838abec1dd23092f06fc0316797a003a
Vinay Uppin <vins.github@gmail.com>
2016-04-16 12:45:35 +0530

    My App

---
+++ AngApp/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title><?php
+	echo "Vinay";
+?>
+</title>
+<link rel="stylesheet" type="text/css" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+</head>
+<body ng-app="AngApp">
+
+    <div ng-include="'templates/nav/header.html'"></div>
+    
+    <script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script type="text/javascript" src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="bower_components/angular/angular.min.js"></script>
+    <script type="text/javascript" src="bower_components/angular-ui-router/release/angular-ui-router.min.js"></script>
+    <script type="text/javascript" src="js/app.js"></script>
+</body>
+</html>
